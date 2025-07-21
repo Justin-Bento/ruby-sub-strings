33d9d4151a76ee44f598f0211e8a225ea4812a13
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-07-21 17:08:17 -0400

    created an array of words

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 sample_text = "below."
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def sub_strings(text, dictionary)
   puts text.chars()
