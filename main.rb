aaf34ad94d8abb0638ddc4410537690815ef7cd0
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-07-21 17:10:35 -0400

    renamed sample text to message

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
-sample_text = "below."
+message = "below."
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def sub_strings(text, dictionary)
   puts text.chars()
 end
 
-sub_strings(sample_text, dictionary)
+sub_strings(message, dictionary)
