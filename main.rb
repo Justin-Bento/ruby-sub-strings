fca446d6829175558e3b8b554c1d5519b8f81e06
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-07-31 17:26:22 -0400

    Used %w[ ] for the dictionary array (RuboCop compliant)

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
 def sub_strings(string, dictionary)
   if dictionary.include?(string)
