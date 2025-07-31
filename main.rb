7c7fda7da39bafb142109d8141b1953656859a56
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-07-31 17:18:33 -0400

    removed message variable as the first arugemnt will grab the user input

--- main.rb
+++ main.rb
@@ -1,12 +1,11 @@
-message = "below"
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def sub_strings(text, dictionary)
   if dictionary.include?(text)
     return true
-  else 
+  else
     return false
   end
 end
 
-puts sub_strings(message, dictionary)
+puts sub_strings("below", dictionary)
