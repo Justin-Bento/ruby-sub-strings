6e754e26da690345d3ba1cceef38489df9bfa024
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-07-31 17:52:27 -0400

    added result

--- main.rb
+++ main.rb
@@ -1,11 +1,23 @@
 dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
-def sub_strings(string, dictionary)
-  if dictionary.include?(string)
-    return true
-  else
-    return false
+def sub_strings(_string, dictionary)
+  puts 'Please Enter a word: '
+  input = gets.chomp.downcase
+
+  result = {}
+
+  dictionary.each do |word|
+    count = input.scan(word.downcase).count
+    result[word] = count if count.positive?
   end
+
+  if result.empty?
+    puts 'There are no substrings that match'
+    exit
+  else
+    result
+  end
+
 end
 
 puts sub_strings("below", dictionary)
