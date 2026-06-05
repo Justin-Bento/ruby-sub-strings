1c657fb5123f758d6b8efaf854c11866312ee871
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2026-06-05 09:10:18 -0400

    feat: add substring dictionary match counter with output

--- sub-strings.rb
+++ sub-strings.rb
@@ -3,6 +3,14 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 def subString(words, dictionary)
   substrings = words.split(' ')
   results = Hash.new(0)
+  substrings.each do |substring|
+    dictionary.each do |word|
+      if substring.include?(word)
+        results[word] += 1
+      end
+    end
+  end
+  puts "#{results}"
 end
 
 subString("Hello", dictionary)
